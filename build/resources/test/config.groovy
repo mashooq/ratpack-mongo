bf791308e5ff3f5081bf9d19148b1e4dbb57760e
Mashooq Badar <mashooq.badar@gmail.com>
2013-08-20 01:05:25 +0100

    added functional & integation testing tasks

--- build/resources/test/config.groovy
+++ build/resources/test/config.groovy
@@ -1,17 +1,3 @@
-import org.ratpackframework.groovy.config.Config
-
-
-(this as Config).with {
-
-	routing.with {
-		reloadable = true
-
-		// Uncomment this to allow for statically compiled Groovy files.
-//		routing.staticallyCompile = true
-	}
-
-}
-
 app {
     mongo {
         host = 'ds037468.mongolab.com'
